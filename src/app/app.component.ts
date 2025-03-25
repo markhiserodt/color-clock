ebc69bfcb2606cb031ad39a5938ed925deb88f8f
Mark Hiserodt <markhiserodt@cypherfusion.com>
2025-03-24 22:41:02 -0700

    colorInTime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+  protected color = '';
 
-  SECONDS_IN_DAY = 8;
-  
-  color = 'rgb(255, 0, 0)';
+  private reds   = [ 255,  -1,   0,   0,  -1, 255];
+  private greens = [  -1, 255, 255,  -1,   0,   0];
+  private blues  = [   0,   0,  -1, 255, 255,  -1];
 
   ngOnInit(): void {
-    setInterval(this.changeBackgroundColor, 1000);
+    this.colorInTime();
+    setInterval(this.colorInTime, 60000);
   }
 
-  changeBackgroundColor = () => {
-    const third = this.SECONDS_IN_DAY/3;
+  private colorInTime = () => {
+    const now = new Date()
+    const hour = now.getHours();
+    const minutes = now.getMinutes();
+    const section = Math.floor(hour / 4);
 
-    this.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    const red = this.reds[section] === -1 ? this.adjustValue(hour, minutes, section) : this.reds[section];
+    const green = this.greens[section] === -1 ? this.adjustValue(hour, minutes, section) : this.greens[section];
+    const blue = this.blues[section] === -1 ? this.adjustValue(hour, minutes, section) : this.blues[section];
+
+    this.color = `rgb(${red}, ${green}, ${blue})`;
+    console.log(`${hour}:${minutes} ${this.color}`);
+  }
+
+  private adjustValue(hour: number, minutes: number, section: number) {
+    return section % 2 === 0 ?
+      (hour - section * 4) * 64 + Math.floor(minutes * (64 / 60)) :
+      255 - ((hour - section * 4) * 64 + Math.ceil(minutes * (64 / 60)))
   }
 
 }
